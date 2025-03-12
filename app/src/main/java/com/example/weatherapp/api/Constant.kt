0a4a3961a64ed35773e05cd9d3439d437fa2b575
Mehedi Hasan <167241899+hasan-mehedii@users.noreply.github.com>
2025-03-12 16:36:42 +0600

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/api/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.weatherapp.api
+
+object Constant {
+    val apiKey = "00bb5d99897748f29bf190758251103"
+}
---
+++ app/src/main/java/com/example/weatherapp/api/NetworkResponse.kt
@@ -0,0 +1,8 @@
+package com.example.weatherapp.api
+
+//T indicates weather model
+sealed class NetworkResponse<out T> {
+    data class Success<out T>(val data: T) : NetworkResponse<T>()
+    data class Error(val message: String) : NetworkResponse<Nothing>()
+    data object Loading : NetworkResponse<Nothing>()
+}
